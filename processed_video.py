0f5e5a5a84602541d5ea6792f624cf07395afa60
Elan456 <telan4892@gmail.com>
2024-03-03 03:55:30 -0500

    full purple box

--- processed_video.py
+++ processed_video.py
@@ -6,6 +6,8 @@ class ProcessedVideo:
         self.segments = []
 
     def create(self, transcript: list[dict], frame_descriptions: list[str]):
+        print("Transcript len: ", len(transcript))
+        print("frame desciritpn len: ", len(frame_descriptions))
         assert (len(transcript) == len(frame_descriptions))
         self.segments = []
         for i, segment in enumerate(transcript):
