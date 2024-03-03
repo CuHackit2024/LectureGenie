43b88d5490f9b2795a6b3eaa198ad326402eeb69
Elan456 <telan4892@gmail.com>
2024-03-03 02:35:42 -0500

    frame description separate server

--- video_processing/keyframe/__main__.py
+++ video_processing/keyframe/__main__.py
@@ -2,20 +2,30 @@ from PIL import Image
 from . import descriptor
 from . import graber
 import time
+from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
     start_time = time.time()
-    frames = graber.timed_frames("sample_video/data_science.mp4", number_frames=50)
+    frames = graber.timed_frames("sample_video/data_science.mp4", number_frames=5)
     time_to_get_frames = time.time() - start_time
     print(f"Time to get frames: {time_to_get_frames}")
 
     get_descriptions_start_time = time.time()
-    my_descriptor = descriptor.Descriptor()
-    descriptions = my_descriptor.generate_descriptions([Image.fromarray(f[1]) for f in frames])
+
+    descriptions = descriptor.get_descriptions([f[1] for f in frames])
     time_to_get_descriptions = time.time() - get_descriptions_start_time
     print(f"Time to get descriptions: {time_to_get_descriptions}")
+
     for i, description in enumerate(descriptions):
         head_index = min(50, len(description))
         print(f"Frame {i + 1}: {description[:head_index]}...")
+
+    # Rendering description, key pairs
+    for i, (frame, description) in enumerate(zip(frames, descriptions)):
+        plt.imshow(frame[1])
+        plt.title(description)
+        plt.show()
+
+
 
     print(f"Total time: {time.time() - start_time}")
