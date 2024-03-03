a0973dbae9ebc7f839f4566e3dee0dc104ed529b
Elan456 <telan4892@gmail.com>
2024-03-03 03:20:33 -0500

    ProcessedVideo class

--- segment.py
+++
@@ -1,9 +0,0 @@
-class Segment:
-    def __init__(self, start, end, text):
-        self.start = start
-        self.end = end
-        self.text = text
-        self.frame_description = None
-
-    def add_keyframe_description(self, description):
-        self.frame_description = description
