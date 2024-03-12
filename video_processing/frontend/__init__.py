2a719e84387f0667273c7fdd720d9eca38a8ba82
Elan456 <telan4892@gmail.com>
2024-03-12 01:05:54 -0400

    Better username initialization

--- video_processing/frontend/__init__.py
+++ video_processing/frontend/__init__.py
@@ -4,13 +4,6 @@ from .transcribe_video_frontend import transcribe_video_frontend
 from .keyframe_processing_frontend import keyframe_processing_frontend
 from .login_frontend import login_frontend
 
-if "video_processing_stage" not in st.session_state:
-    st.session_state["video_processing_stage"] = "upload_video"
-if "processed_video" not in st.session_state:
-    st.session_state["processed_video"] = None
-if "username" not in st.session_state:
-    st.session_state["username"] = None
-
 
 def start_over():
     st.session_state["video_processing_stage"] = "upload_video"
@@ -18,14 +11,18 @@ def start_over():
 
 
 def process_video_frontend():
-    if st.session_state["video_processing_stage"] is None:
+    if "video_processing_stage" not in st.session_state:
         st.session_state["video_processing_stage"] = "upload_video"
+    if "processed_video" not in st.session_state:
+        st.session_state["processed_video"] = None
+    if "username" not in st.session_state:
+        st.session_state["username"] = None
+
     # Container for the video processing frontend
     video_processing_container = st.sidebar.container()
     with video_processing_container:
         login_frontend()
         if st.session_state["video_processing_stage"] == "upload_video" and st.session_state["username"] is not None:
-            print("uploading video")
             upload_video_frontend()
 
         if st.session_state["video_processing_stage"] == "transcribe_video":
